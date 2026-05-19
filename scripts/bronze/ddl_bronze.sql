ec518119d622ad1aa18c1a4c995658c98880f296
chaitrahr2020-hue <chaitrahr2020@gmail.com>
2026-05-19 22:24:00 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,67 +8,76 @@ Script Purpose:
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
--- Drop the database if it exists
-DROP DATABASE IF EXISTS DataWarehouse;
 
-    
--- Create the 'DataWarehouse' database
-create database DataWarehouse;
-use datawarehouse;
+-- crm_cust_info
+DROP TABLE IF EXISTS bronze.crm_cust_info;
 
--- Create Schemas
-create schema bronze;
-create schema silver;
-create schema gold;
-
--- create bronze layer 
 CREATE TABLE bronze.crm_cust_info (
-    cst_id              INT,
-    cst_key             VARCHAR(50),
-    cst_firstname       VARCHAR(50),
-    cst_lastname        VARCHAR(50),
-    cst_marital_status  VARCHAR(50),
-    cst_gndr            VARCHAR(50),
-    cst_create_date     DATE
+    cst_id INT,
+    cst_key VARCHAR(50),
+    cst_firstname VARCHAR(50),
+    cst_lastname VARCHAR(50),
+    cst_marital_status VARCHAR(50),
+    cst_gndr VARCHAR(50),
+    cst_create_date DATE
 );
+
+
+-- crm_prd_info
+DROP TABLE IF EXISTS bronze.crm_prd_info;
 
 CREATE TABLE bronze.crm_prd_info (
-    prd_id       INT,
-    prd_key      VARCHAR(50),
-    prd_nm       VARCHAR(50),
-    prd_cost     INT,
-    prd_line     VARCHAR(50),
+    prd_id INT,
+    prd_key VARCHAR(50),
+    prd_nm VARCHAR(50),
+    prd_cost INT,
+    prd_line VARCHAR(50),
     prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
+    prd_end_dt DATETIME
 );
+
+
+-- crm_sales_details
+DROP TABLE IF EXISTS bronze.crm_sales_details;
 
 CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num  VARCHAR(50),
-    sls_prd_key  VARCHAR(50),
-    sls_cust_id  INT,
+    sls_ord_num VARCHAR(50),
+    sls_prd_key VARCHAR(50),
+    sls_cust_id INT,
     sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
     sls_quantity INT,
-    sls_price    INT
+    sls_price INT
 );
-drop TABLE if exists bronze.erp_loc_a101 ;
+
+
+-- erp_loc_a101
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+
 CREATE TABLE bronze.erp_loc_a101 (
-    cid    VARCHAR(50),
-    cntry  VARCHAR(50)
+    cid VARCHAR(50),
+    cntry VARCHAR(50)
 );
+
+
+-- erp_cust_az12
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
 
 CREATE TABLE bronze.erp_cust_az12 (
-    cid    VARCHAR(50),
-    bdate  DATE,
-    gen    VARCHAR(50)
+    cid VARCHAR(50),
+    bdate DATE,
+    gen VARCHAR(50)
 );
 
-drop TABLE if exists bronze.erp_px_cat_g1v2;
+
+-- erp_px_cat_g1v2
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id           VARCHAR(50),
-    cat          VARCHAR(50),
-    subcat       VARCHAR(50),
-    maintenance  VARCHAR(50)
+    id VARCHAR(50),
+    cat VARCHAR(50),
+    subcat VARCHAR(50),
+    maintenance VARCHAR(50)
 );
